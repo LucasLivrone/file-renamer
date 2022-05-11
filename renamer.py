dae9582ab02ab1b0efff26499c9b137e6507d27c
Lucas Livrone <lucas.a.livrone@tsgforce.com>
2022-05-11 20:56:38 -0300

    Added path and file selection on runtime

--- renamer.py
+++ renamer.py
@@ -1,37 +1,40 @@
-
 import os
 
 
-PATH = 'path-to-files/'
-NAMES_FILE = 'file-with-names'
-
-
 def main():
-    actual_names = get_actual_names()
-    new_names = get_new_names()
+    actual_names_path, new_names_file = get_path_and_file()
+    actual_names = get_actual_names(actual_names_path)
+    new_names = get_new_names(new_names_file)
     if len(actual_names) == len(new_names):
-        rename(actual_names, new_names)
+        rename(actual_names_path, actual_names, new_names)
         quit("Rename was done.")
     else:
         quit("Actual and New names quantities don't match.")
 
 
-def get_actual_names():
+def get_path_and_file():
+    print("\nWelcome to File-Renamer\n")
+    actual_names_path = input("Select files path: ")
+    new_names_file = input("Select name file: ")
+    return actual_names_path, new_names_file
+
+
+def get_actual_names(actual_names_path):
     file_dictionary = dict()
-    for file in os.listdir(PATH):
-        if "lesson" in file:
-            key = int(file.replace('lesson','').replace('.mp4',''))
-            file_dictionary.update({key:file})  # Generated dictionary will have { 1: 'video1.mp4', 10: 'video10.mp4, ... , 2: 'video2.mp4', ... }
+    for file in os.listdir(actual_names_path):
+        if "video" in file:
+            key = int(file.replace('video','').replace('.mp4',''))
+            file_dictionary.update({key:file})  # Example: generated dictionary will have { 1: 'video1.mp4', 10: 'video10.mp4, ... , 2: 'video2.mp4', ... }
 
     file_list = list()
     for i in range(1,len(file_dictionary)+1):
-        file_list.append(file_dictionary[i])  # Generated list will have ['video1.mp4','video2.mp4', ... ,'video10.mp4', ... ]
+        file_list.append(file_dictionary[i])  # Example: generated list will have ['video1.mp4','video2.mp4', ... ,'video10.mp4', ... ]
 
     return file_list
 
 
-def get_new_names():
-    with open(PATH+NAMES_FILE) as f:
+def get_new_names(new_names_file):
+    with open(new_names_file) as f:
         names = f.read().splitlines()  # Creates a list with all lines from the NAMES_FILE.
     names = names_sanitization(names)  
     return names
@@ -48,10 +51,10 @@ def names_sanitization(names):
     return names
 
 
-def rename(files, names):
-    for file, name in zip(files,names):
-        #print(f'The file {file} will be renamed to: {name+".mp4"}')  # Can be used to check if the final result is the expected one.
-        os.rename(PATH+file,PATH+name+'.mp4')
+def rename(actual_names_path, actual_names, new_names):
+    for actual_name, new_name in zip(actual_names,new_names):
+        #print(f'The file {actual_name} will be renamed to: {new_name+".mp4"}')  # Can be used to check if the final result is the expected one.
+        os.rename(actual_names_path+actual_name, actual_names_path+new_name+'.mp4')
 
 
 if __name__ == '__main__':
